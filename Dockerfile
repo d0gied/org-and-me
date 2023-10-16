9929fb66cd051f17cf5f4c7a4fca17306a4cd768
d0gied <german.n.u@mail.ru>
2023-10-16 17:39:06 +0300

    reorganize docker file to speed up build

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM python:3.11.2-slim
 
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
-COPY . /app
-
 RUN pip install poetry
 
+# Copy the current directory contents into the container at /app
+COPY poetry.lock pyproject.toml /app/
 RUN poetry config virtualenvs.create false && poetry install --only main --no-interaction --no-ansi
+
+COPY . /app
 
 CMD ["python", "main.py"]
--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,11 +1,13 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+# Main menu
 main_menu = InlineKeyboardBuilder()
 main_menu.button(text="Button 1", callback_data="menu1")
 main_menu.button(text="Button 2", callback_data="menu2")
 main_menu.button(text="Button 3", callback_data="menu3")
 main_menu = main_menu.adjust(1, 1, 1).as_markup()  # width of each row
 
+# Back button to start menu
 to_main_menu = InlineKeyboardBuilder()
 to_main_menu.button(text="Back", callback_data="main_menu")
 to_main_menu = to_main_menu.as_markup()
